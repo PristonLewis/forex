18545723e6dc9474dcc283b92cd45bd9df008f38
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-11 11:44:32 +0530

    defined folder structure

---
+++ src/app/main/main-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FundTransferComponent } from './components/fund-transfer/fund-transfer.component';
+const routes: Routes = [
+  { path: 'fundtransfer', component: FundTransferComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule { }
