fa1c5c5ed99b33f576de83de6c6f6fac120cbd51
krishna <krishnakuma@hcl.com>
2020-02-11 12:31:18 +0530

    login

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FundTransferComponent } from './components/fund-transfer/fund-transfer.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [FundTransferComponent],
+  declarations: [FundTransferComponent, LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MainModule { }
