f055bcccf6021ee4a4e638ee082b0b3e2180eea4
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-11 11:23:27 +0530

    defined folder structure

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FundTransferComponent } from './components/fund-transfer/fund-transfer.component';
+
+
+
+@NgModule({
+  declarations: [FundTransferComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
