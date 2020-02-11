7da13c9620277a93faf33199c76bd882da81aaf9
user1 <user1@LP-5CD9296CX3.HCLT.CORP.HCL.IN>
2020-02-11 17:56:14 +0530

    Authguard changes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { FundTransferComponent } from './components/fund-transfer/fund-transfer.component';
 import { LoginComponent } from './components/login/login.component';
 import { TranshistoryComponent } from './components/transhistory/transhistory.component'
-
+import { AuthGuardService as AuthGuard } from '../shared/services/auth-guard.service';
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'transhistory', component: TranshistoryComponent },
-  { path: 'fundtransfer', component: FundTransferComponent }
+  { path: 'transhistory', component: TranshistoryComponent, canActivate: [AuthGuard] },
+  { path: 'fundtransfer', component: FundTransferComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
