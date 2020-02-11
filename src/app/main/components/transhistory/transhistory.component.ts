a7c68e961e17632d951e14484f237dc86ddf2670
krishna <krishnakuma@hcl.com>
2020-02-11 15:14:44 +0530

    transhistory

---
+++ src/app/main/components/transhistory/transhistory.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transhistory',
+  templateUrl: './transhistory.component.html',
+  styleUrls: ['./transhistory.component.sass']
+})
+export class TranshistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
